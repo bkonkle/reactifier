06e5ea056bbf628915c49bf7344854aaadf56163
Brandon Konkle <brandon@konkle.us>
2015-09-15 17:12:09 -0600

    Don't forget the .com!

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -12,7 +12,7 @@ export default class Head extends Component {
         <meta name="description" content="A blog post aggregator bringing together React developers from across the globe."/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="stylesheet" href="media/css/style.css"/>
-        <link rel="alternate" type="application/rss+xml" title="RSS Feed" href="http://reactifier/rss.xml"/>
+        <link rel="alternate" type="application/rss+xml" title="RSS Feed" href="http://reactifier.com/rss.xml"/>
       </head>
     );
   }
